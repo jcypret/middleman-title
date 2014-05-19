e83bcf279ebcee04c425779581522a30eb9b8bbd
Justin Cypret <jcypret@gmail.com>
2014-05-19 17:30:16 -0500

    Refactor for consistency

--- lib/middleman-title/helpers.rb
+++ lib/middleman-title/helpers.rb
@@ -2,9 +2,9 @@ module Middleman
 	module Title
 		module Helpers
 
-
 			def page_title
-				title = [current_page_title]
+				title = []
+				title = add_page_name_to_title(title)
 				title = add_website_name_to_title(title)
 				title.compact.join(title_separator)
 			end
@@ -15,41 +15,47 @@ module Middleman
 				end
 
 				def website_name
-					options[:name] || nil
+					current_page.data.title_site || options[:site]
 				end
 
 				def title_separator
 					options[:separator] || ' &mdash; '
 				end
 
-				def current_page_title
-					current_page_title = current_page.data.title
-					if current_page_title.is_a? Array
-						current_page_title = current_page_title.join(title_separator)
+				def title_reverse
+					current_page.data.title_reverse || options[:reverse]
+				end
+
+				def page_name
+					page_name = current_page.data.title
+
+					if page_name.is_a? Array
+						page_name = page_name.join(title_separator)
 					end
-					current_page_title
+
+					page_name
 				end
 
 				def add_website_name_to_title(title)
-					if website_name_first?
-						title.unshift(website_name)
-					elsif hide_website_name?
+					if website_name.nil? || current_page.data.title_site == false
 						title
+					elsif website_name_first?
+						title.unshift(website_name)
 					else
 						title << website_name
 					end
 				end
 
-				def current_page_title?
-					current_page.data.title.present?
+				def add_page_name_to_title(title)
+					title << page_name
 				end
 
 				def website_name_first?
-					options[:reverse] || current_page.data.title_reverse == true
+					title_reverse
 				end
 
 				def hide_website_name?
-					current_page.data.title_name == false
+					current_page.data.title_site == false
 				end
 
 
