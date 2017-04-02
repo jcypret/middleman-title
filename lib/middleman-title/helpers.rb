17f011eedfe8438e20206d8f658ca3db0ee66d9b
Justin Cypret <jcypret@gmail.com>
2017-04-01 21:15:18 -0500

    Fix indention of private methods

--- lib/middleman-title/helpers.rb
+++ lib/middleman-title/helpers.rb
@@ -13,53 +13,54 @@ module Middleman
       end
 
       private
-        def title_options
-          ::Middleman::TitleExtension.options
+
+      def title_options
+        ::Middleman::TitleExtension.options
+      end
+
+      def website_name
+        current_page.data.title_site || title_options.site
+      end
+
+      def title_separator
+        title_options.separator
+      end
+
+      def title_reverse
+        if current_page.data.title_reverse.nil? == false
+          current_page.data.title_reverse
+        else
+          title_options.reverse
+        end
+      end
+
+      def page_name
+        page_name = current_page.data.title
+
+        if page_name.is_a? Array
+          page_name = page_name.join(title_separator)
         end
 
-        def website_name
-          current_page.data.title_site || title_options.site
+        page_name
+      end
+
+      def add_website_name_to_title(title)
+        if current_page.data.title_site == false || website_name == false
+          title
+        elsif website_name_first?
+          title.unshift(website_name)
+        else
+          title << website_name
         end
+      end
 
-        def title_separator
-          title_options.separator
-        end
+      def add_page_name_to_title(title)
+        title << page_name
+      end
 
-        def title_reverse
-          if current_page.data.title_reverse.nil? == false
-            current_page.data.title_reverse
-          else
-            title_options.reverse
-          end
-        end
-
-        def page_name
-          page_name = current_page.data.title
-
-          if page_name.is_a? Array
-            page_name = page_name.join(title_separator)
-          end
-
-          page_name
-        end
-
-        def add_website_name_to_title(title)
-          if current_page.data.title_site == false || website_name == false
-            title
-          elsif website_name_first?
-            title.unshift(website_name)
-          else
-            title << website_name
-          end
-        end
-
-        def add_page_name_to_title(title)
-          title << page_name
-        end
-
-        def website_name_first?
-          title_reverse
-        end
+      def website_name_first?
+        title_reverse
+      end
 
     end
   end
